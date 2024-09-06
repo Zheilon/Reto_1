cf47d4c3a313639604087322761ffe21e2be98a0
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-06 15:52:40 -0500

    Panel / Proyect

---
+++ work.py
@@ -0,0 +1,53 @@
+import math
+
+print()
+#EnvironmentVariables
+annualConsuption = 12000
+averageLight = 5 #horas
+panelEffiency = 0.15
+panelSurface = 1.6
+#averageRadiation = averageLight / panelSurface / 1
+averageRadiation = 5
+
+#Daily Power
+dailyPower = (panelSurface) * (averageRadiation) * (panelEffiency)
+print(f"Potencia Diaria: {dailyPower} Kwh")
+
+#Saber el uso de las funciones.
+#Ceil
+
+#Annual Power
+annualPower = dailyPower * 365 
+print(f"Potencia Anual: {annualPower} Kwh")
+
+#Number Of Panels. Round to 1 decimal.
+numberOfPanels = (annualConsuption / annualPower)
+print(f"Número Paneles: {math.ceil(numberOfPanels)}")
+
+print("Extra")
+print()
+
+newAnnualConsuption = float(input("Ingresa Consumo Anual: "))
+#-------------------------------
+newPanelEfficiency = float(input("Ingresa Eficiencia del Panel: "))
+#-------------------------------
+newPanelSurface = float(input("Ingresa Superficie Promedio del Panel: "))
+#-------------------------------
+newAverageRadiation = float(input("Ingresa Radiación Solar Promedio: "))
+#-------------------------------
+newAverageLight = float(input("Ingresa Horas Promedio de Sol / Día: "))
+
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
